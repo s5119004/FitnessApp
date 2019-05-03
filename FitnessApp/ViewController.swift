e72299ae7159b7e64b5f9313d500af272fe3e933
Daniel Dodds (s5119004) <s5119004@mac42938.mac.bournemouth.ac.uk>
2019-05-03 10:33:00 +0100

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by Daniel Dodds (s5119004) on 03/05/2019.
+//  Copyright © 2019 Daniel Dodds (s5119004). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
