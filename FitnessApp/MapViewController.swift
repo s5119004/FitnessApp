ef775f5e58a6d3150404080980cf25b9f7e8103d
Olivia Irwin (s5113143) <s5113143@mac42945.mac.bournemouth.ac.uk>
2019-05-03 10:52:37 +0100

    delete te back button coding

--- FitnessApp/MapViewController.swift
+++ FitnessApp/MapViewController.swift
@@ -8,12 +8,6 @@ class MapViewController: UIViewController {
 
     var hasPutPoints = false
     
-    //@IBAction func BackButton(_ sender: UIBarButtonItem){
-      // dismiss(animated:true, completion: nil)
-       // func setCustomBackImage(){
-        //navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
-      //  }
-   // }
     @IBOutlet weak var MapView: MKMapView!
     
     override func viewDidLoad() {
