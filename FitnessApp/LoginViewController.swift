4b5e4cafa31246dcb9e01c1df559863020dca2ce
Monica Cruz (s5107958) <s5107958@mac38582.mac.bournemouth.ac.uk>
2019-05-08 10:41:27 +0100

    outlet connecting to new controller

---
+++ FitnessApp/LoginViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func login(_ sender: Any) {
+    }
+    
+}
+
+    
+
+
+
+
+
