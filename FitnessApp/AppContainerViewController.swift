4e0924fecf63e3e0b4d40bb6f216fcc975288536
Monica Cruz (s5107958) <s5107958@mac38582.mac.bournemouth.ac.uk>
2019-05-08 11:18:54 +0100

    AppManager.swift

--- FitnessApp/AppContainerViewController.swift
+++ FitnessApp/AppContainerViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
-class AppContainerController: UIInputViewController {
+class AppContainerViewController: UIInputViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-            
+         AppManager.shared.appContainer = self
+        AppManager.shared.showApp()
         }
     
 }
--- FitnessApp/AppManager.swift
+++ FitnessApp/AppManager.swift
@@ -6,7 +6,7 @@ class AppManager {
     static let shared = AppManager()
     
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    var appContainer = AppContainerController!
+    var appContainer: AppContainerViewController!
     
     
     
@@ -14,5 +14,25 @@ class AppManager {
     
     private init() {}
     
+    func showApp() {
+        
+        var SecondViewController: UIViewController
+        
+        if Auth.auth().currentUser == nil {
+        //firebase way getting the cashed of the users detieleds
+            SecondViewController = storyboard.instantiateViewController(withIdentifier: "loginViewController")
+        } else {
+            SecondViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+            
+        }
+        
+            
+        appContainer.present(SecondViewController, animated: true, completion: nil)
+        }
+    func logout() {
+        try!Auth.auth().signOut()
+        appContainer.presentedViewController?.dismiss(animated: true, completion: nil)
+    }
+    }
     
-}
+
