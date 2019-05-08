e77d19b6763431cce3fca5b326f111ea464aa73a
Miles Darby (s5116926) <s5116926@mac38581.mac.bournemouth.ac.uk>
2019-05-08 10:58:30 +0100

    setting up auth
    2 errors have appeared on app mananger stage

--- FitnessApp/AppContainerViewController.swift
+++ FitnessApp/AppContainerViewController.swift
@@ -1,1 +1,11 @@
+import UIKit
+
+class AppContainerController: UIInputViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+            
+        }
+    
+}
 
---
+++ FitnessApp/AppManager.swift
@@ -0,0 +1,18 @@
+import UIKit
+import Firebase
+
+class AppManager {
+    
+    static let shared = AppManager()
+    
+    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    var appContainer = AppContainerController!
+    
+    
+    
+    
+    
+    private init() {}
+    
+    
+}
